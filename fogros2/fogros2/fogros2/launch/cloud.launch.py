03652d5ef2e6606d189d22b734f44add829a0f58
Kaiyuan Chen <chenkaiyuan@ucla.edu>
2022-02-05 21:37:21 -0800

    successfully push the talker node to the cloud

--- fogros2/fogros2/fogros2/launch/cloud.launch.py
+++ fogros2/fogros2/fogros2/launch/cloud.launch.py
@@ -7,13 +7,13 @@ import pickle
 
 def generate_launch_description():
     ld = LaunchDescription()
-    node_dir = "/home/ubuntu/ros2_ws/src/fogros2/fogros2"
-    node_files = [os.path.join(node_dir, file) for file in os.listdir(node_dir) if file.startswith("to_cloud")]
-    print(node_files)
-    for node in node_files:
-        with open(node, "rb") as f:
-            node_in_str = f.read()
-            node = pickle.loads(node_in_str)
+    node_path = "/home/ubuntu/ros2_ws/src/fogros2/fogros2/to_cloud_nodes"
+    #[os.path.join(node_dir, file) for file in os.listdir(node_dir) if file.startswith("to_cloud")]
+    with open(node_path, "rb") as f:
+        nodes_in_str = f.read()
+    nodes = pickle.loads(nodes_in_str)
+    for node in nodes:
         ld.add_action(node)
+        print("action added")
     return ld
 
