8ce60cb5442e8e63ab4f50afa4e4253f968a9d06
Kaiyuan Chen <chenkaiyuan@ucla.edu>
2022-02-05 16:12:36 -0800

    get scp & ssh working

--- fogros2/fogros2/fogros2/fogros2.py
+++ fogros2/fogros2/fogros2/fogros2.py
@@ -2,6 +2,7 @@
 from launch_ros.actions import Node
 import pickle
 from .aws import AWS
+from .comm import SCP_Client
 import logging
 
 def main():
@@ -10,8 +11,14 @@ def main():
     node = pickle.loads(node_as_str)
     print('Hi from fogros2.')
 
-    aws_instance = AWS()
-    aws_instance.create()
+    # aws_instance = AWS()
+    # aws_instance.create()
+    ip = "13.57.18.68"
+    key_path = "/opt/ros2_ws/FogROSKEY677.pem"
+    scp = SCP_Client(ip, key_path)
+    scp.execute_cmd("echo hello")
+
+
 
 
 if __name__ == '__main__':
