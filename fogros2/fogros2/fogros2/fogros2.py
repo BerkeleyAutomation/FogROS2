bcd8ea309eb033974b27b2a582c3f46f9fbb32be
Kaiyuan Chen <chenkaiyuan@ucla.edu>
2022-02-05 16:06:17 -0800

    Launch EC2 instances

--- fogros2/fogros2/fogros2/fogros2.py
+++ fogros2/fogros2/fogros2/fogros2.py
@@ -1,12 +1,17 @@
 
 from launch_ros.actions import Node
 import pickle
+from .aws import AWS
+import logging
 
 def main():
     with open("/opt/ros2_ws/src/fogros2/fogros2/test_node", "rb") as f:
         node_as_str = f.read()
     node = pickle.loads(node_as_str)
     print('Hi from fogros2.')
+
+    aws_instance = AWS()
+    aws_instance.create()
 
 
 if __name__ == '__main__':
--- fogros2/fogros2/setup.py
+++ fogros2/fogros2/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'fogros2'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('cloud_providers/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
