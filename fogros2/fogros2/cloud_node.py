70ab30ef49707a978c02279658e1bd4d743d418b
Kaiyuan Chen <chenkaiyuan@ucla.edu>
2022-02-15 11:51:04 -0800

    make pickle not serializing CloudNode.machine, otherwise trigger unserializable error

--- fogros2/fogros2/cloud_node.py
+++ fogros2/fogros2/cloud_node.py
@@ -11,5 +11,11 @@ class CloudNode(Node):
         super().__init__(**kwargs)
         self.machine = machine
 
+    def __getstate__(self):
+        # workaround to make pickle not serializing self.machine 
+        state = self.__dict__.copy()
+        del state["machine"]
+        return state
+
     def get_unique_id(self):
         return self.machine.get_name()
