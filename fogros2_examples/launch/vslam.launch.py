ac594c70994f7de5792d65c8e9d2a0d45ff8a3f3
Eric Chen <kych@berkeley.edu>
2022-03-01 02:21:28 -0800

    add local command

--- fogros2_examples/launch/vslam.launch.py
+++ fogros2_examples/launch/vslam.launch.py
@@ -18,6 +18,10 @@ def generate_launch_description():
     ld = FogROSLaunchDescription()
     machine1 = fogros2.AWS(region="us-west-1", ec2_instance_type="t2.medium", ami_image="ami-09175f2ca3c3dc67c")
 
+
+    #run this command locally:
+    # sudo docker run --net=host --env RMW_IMPLEMENTATION=rmw_cyclonedds_cpp --env CYCLONEDDS_URI=file:///tmp/cyclonedds.xml -v $(pwd)/install/share/fogros2/configs/cyclonedds.xml:/tmp/cyclonedds.xml --rm -it -v /home/gdpmobile7/rgbd_dataset_freiburg1_xyz:/dataset -v $(pwd)/output:/output mjd3/orbslam-ros ros2 launch orb_slam2_ros orb_slam2_d435_rgbd_client_launch.py dataset:=/dataset compress:=0
+    
     machine1.add_docker_container("sudo docker run --net=host --env RMW_IMPLEMENTATION=rmw_cyclonedds_cpp --env CYCLONEDDS_URI=file:///tmp/cyclonedds.xml -v /home/ubuntu/cyclonedds.xml:/tmp/cyclonedds.xml --rm -it mjd3/orbslam-ros ros2 launch orb_slam2_ros orb_slam2_d435_rgbd_launch.py compress:=0")
     #keplerc/gqcnn_ros:pj ros2 launch gqcnn_ros planner.launch.py")
 
